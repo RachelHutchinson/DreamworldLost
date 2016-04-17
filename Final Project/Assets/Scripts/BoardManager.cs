e8755e54ce49f10dce9d8042caa19db1e17b56c3
RachelHutchinson <RachelHutchinson@Rachels-iMac.local>
2016-04-16 22:02:26 -0400

    Second room active

--- Final Project/Assets/Scripts/BoardManager.cs
+++ Final Project/Assets/Scripts/BoardManager.cs
@@ -5,10 +5,10 @@ using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour {
 
-	public int innerColumns = 9;
-	public int innerRows = 9;
-	public int outerColumns = 24;
-	public int outerRows = 24;
+	public int innerColumns = 10;
+	public int innerRows = 10;
+	public int outerColumns = 60;
+	public int outerRows = 50;
 	public GameObject path;
 	public GameObject [] innerTiles;
 	public GameObject [] outerTiles;
@@ -16,39 +16,33 @@ public class BoardManager : MonoBehaviour {
 	public GameObject [] outerWalls;
 
 	private Transform boardHolder;
-	private Transform roomHolder;
 	private List <Vector3> gridPositions = new List <Vector3> ();
 
 
 	void BoardSetup()
 	{
 		boardHolder = new GameObject ("Board").transform;
-		for (int x = -1; x < outerColumns + 1; x++) {
-			for (int y = -1; y < outerRows + 1; y++) {
+		for (int x = -1; x < innerColumns; x++) {
+			for (int y = -1; y < innerRows; y++) {
+				GameObject toInstantiate = innerTiles [Random.Range (0, innerTiles.Length)];
+				if (x == -1 || x == innerColumns || y == -1|| y == innerRows)
+					toInstantiate = innerWalls [Random.Range (0, innerWalls.Length)];
+				GameObject instance =
+					Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+			}
+		}
+		for (int x = 8; x < outerColumns + 1; x++) {
+			for (int y = -20; y < outerRows + 1; y++) {
 				GameObject toInstantiate = outerTiles [Random.Range (0, outerTiles.Length)];
-				if (x == -1 || x == outerColumns || y == -1 || y == outerRows)
+				if (x == 8|| x == outerColumns || y == -20 || y == outerRows)
 					toInstantiate = outerWalls [Random.Range (0, outerWalls.Length)];
 				GameObject instance =
 					Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
 				instance.transform.SetParent (boardHolder);
 			}
-	}
-	}
-
-	void RoomSetup()
-	{
-		roomHolder = new GameObject ("Room").transform;
-		for (int x = innerColumns; x < innerColumns+ 6; x++) {
-			for (int y = innerRows; y < innerRows + 6; y++) {
-				GameObject toInstantiate = innerTiles [Random.Range (0, innerTiles.Length)];
-				if (x == innerColumns || x == innerColumns + 5 || y == innerRows || y == innerRows + 5)
-					toInstantiate = innerWalls [Random.Range (0, innerWalls.Length)];
-				GameObject instance =
-					Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
-				instance.transform.SetParent (roomHolder);
-			}
 		}
 	}
+
 		
 
  public class Count
