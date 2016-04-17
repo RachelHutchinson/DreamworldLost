80a642bf0a67a682fb2b0ab4a027f4bc7c75025d
RachelHutchinson <RachelHutchinson@Rachels-iMac.local>
2016-04-16 20:31:36 -0400

    Getting Started with Board Manager

---
+++ Final Project/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+	public int innerColumns = 8;
+	public int innerRows = 8;
+	public int outerColumns = 24;
+	public int outerrows = 24;
+	public GameObject path;
+	public GameObject [] innerTiles;
+	public GameObject [] outerTiles;
+	public GameObject [] innerWalls;
+	public GameObject [] outerWalls;
+
+	public class count
+	{
+		public int minimum;
+		public int maximum;
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+		
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
