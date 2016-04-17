62d174f57099b751e5fe2ecfc117a7fef944b1c4
RachelHutchinson <RachelHutchinson@Rachels-iMac.local>
2016-04-16 21:06:31 -0400

    First Attempt at BoardManager

--- Final Project/Assets/Scripts/BoardManager.cs
+++ Final Project/Assets/Scripts/BoardManager.cs
@@ -8,31 +8,67 @@ public class BoardManager : MonoBehaviour {
 	public int innerColumns = 8;
 	public int innerRows = 8;
 	public int outerColumns = 24;
-	public int outerrows = 24;
+	public int outerRows = 24;
 	public GameObject path;
 	public GameObject [] innerTiles;
 	public GameObject [] outerTiles;
 	public GameObject [] innerWalls;
 	public GameObject [] outerWalls;
 
-	public class count
+	private Transform boardHolder;
+	private List <Vector3> gridPositions = new List <Vector3> ();
+
+
+	void BoardSetup()
+	{
+		boardHolder = new GameObject ("Board").transform;
+		for (int x = -1; x < outerColumns + 1; x++) {
+			for (int y = -1; y < outerRows + 1; y++) {
+				GameObject toInstantiate = outerTiles [Random.Range (0, outerTiles.Length)];
+				if (x == -1 || x == outerColumns || y == -1 || y == outerRows)
+					toInstantiate = outerWalls [Random.Range (0, outerWalls.Length)];
+				GameObject instance =
+					Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+				instance.transform.SetParent (boardHolder);
+			}
+		}
+	}
+
+ public class Count
 	{
 		public int minimum;
 		public int maximum;
 		public Count (int min, int max)
 		{
-			minimum = min;
-			maximum = max;
+			minimum=min;
+			maximum=max;
 		}
 	}
-		
 
-	void Start () {
-	
+	Vector3 RandomPosition ()
+	{
+		int randomIndex = Random.Range (0, gridPositions.Count);
+		Vector3 randomPosition = gridPositions [randomIndex];
+		gridPositions.RemoveAt(randomIndex);
+		return randomPosition;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		
+	void LayoutObjectAtRandom (GameObject [] outerTileArray, int minimum, int maximum)
+	{
+		int objectCount = Random.Range (minimum, maximum + 1);
+		for(int i=0; i<objectCount; i++)
+			{
+				Vector3 randomPosition = RandomPosition ();
+				GameObject tileChoice = outerTileArray [Random.Range (0, outerTileArray.Length)];
+				Instantiate(tileChoice, randomPosition, Quaternion.identity);
+			}
 	}
 }
+
+
+	/*public void SetupScene (int level)
+	{
+		BoardSetpup ();
+		InitialiseList ();
+	}*/
+
