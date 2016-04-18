59c750f2d87109c67044fb83b485456cc64b24b3
RachelHutchinson <rachelhutchinson@bennington.edu>
2016-04-18 15:15:18 -0400

    All for you

--- Final Project/Assets/Scripts/BoardManager.cs
+++ Final Project/Assets/Scripts/BoardManager.cs
@@ -9,9 +9,8 @@ public class BoardManager : MonoBehaviour {
 	public int innerRows = 10;
 	public int outerColumns = 60;
 	public int outerRows = 50;
-	public int pathWaysColumns = 10;
-	public int pathWaysRows = 10;
-	public GameObject [] pathTiles;
+	public GameObject player;
+	public GameObject path;
 	public GameObject [] innerTiles;
 	public GameObject [] outerTiles;
 	public GameObject [] innerWalls;
@@ -45,17 +44,14 @@ public class BoardManager : MonoBehaviour {
 				instance.transform.SetParent (boardHolder);
 			}
 		}
-		/*//path
-		for (int x = -1; x < pathWaysColumns + 1; x++) {
-			for (int y = -1; y < pathWaysRows + 1 ; y++) {
-				GameObject toInstantiate = pathTiles [Random.Range (0, pathTiles.Length)];
-				if (x == -1 || x == outerColumns || y == -1 || y == outerRows)
-					toInstantiate = pathTiles [Random.Range (0, pathTiles.Length)];
-				GameObject instance =
-					Instantiate (toInstantiate, new Vector3 (0, 0, 0f), Quaternion.identity) as GameObject;
-				instance.transform.SetParent (boardHolder);
-			}
-		}*/
+
+	}
+
+	void playerNPC()
+	{
+		//Player
+		GameObject playerI =
+			Instantiate (player, new Vector3 (0, 0, 0f), Quaternion.identity) as GameObject;
 	}
 
 
