de9a938eb6f02ac4333abecde14f0aeeed5f523f
RachelHutchinson <RachelHutchinson@Rachels-iMac.local>
2016-04-20 21:45:47 -0400

    Trying to Move and Animate

---
+++ Final Project/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MovingObject {
+
+
+	private Animator animator;
+
+	protected override void Start () 
+	{
+		animator = GetComponent<Animator> ();
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+		//if (!GameManager.instance.playersTurn) return;
+		int horizontal = 0;
+		int vertical = 0;
+		horizontal = (int)(Input.GetAxisRaw ("Horizontal"));
+		vertical = (int)(Input.GetAxisRaw ("Vertical"));
+		if (horizontal != 0) 
+		{
+			vertical = 0;
+		}
+	
+	}
+
+	protected override void AttemptMove <T> (int xDir, int yDir)
+	{
+		base.AttemptMove <T> (xDir, yDir);
+		RaycastHit2D hit;
+		/*if (AttemptMove (xDir, yDir, outhit))
+			CheckIfGameOver ();
+		GameManager.instance.playersTurn = false;*/
+	}
+
+	/*protected override void OnCantMove <T> (T component)
+	{
+	}*/
+
+	private void Restart ()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
+
