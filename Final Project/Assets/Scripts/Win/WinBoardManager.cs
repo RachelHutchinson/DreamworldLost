d31c86efec48524ef59077dc4cb40c3f4940f639
RachelHutchinson <RachelHutchinson@Rachels-iMac.local>
2016-05-28 20:08:18 -0400

    Starting to Implement Win

---
+++ Final Project/Assets/Scripts/Win/WinBoardManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class WinBoardManager : MonoBehaviour {
+
+	public int innerColumns = 10;
+	public int innerRows = 10;
+	public GameObject player;
+	public GameObject owl;
+	public GameObject nurse;
+	public GameObject music;
+	public GameObject librarian;
+	public GameObject girlTwin;
+	public GameObject boyTwin;
+	public GameObject [] innerTiles;
+	public GameObject [] innerWalls;
+	public AudioClip innerRoom;
+
+	private Transform boardHolder2;
+	private List <Vector3> gridPositions = new List <Vector3> ();
+
+	void BoardSetup()
+	{
+		boardHolder2 = new GameObject ("Board").transform;
+		//Room 1
+		for (int x = -1; x < innerColumns + 1; x++) {
+			for (int y = -1; y < innerRows + 1; y++) {
+				if (x == 8 && y == 3) {
+					continue;
+				}
+				GameObject toInstantiate = innerTiles[Random.Range (0, innerTiles.Length)];
+				if (x == -1 || x == innerColumns || y == -1 || y == innerRows)
+					toInstantiate = innerWalls[Random.Range (0, innerTiles.Length)];
+				GameObject instance =
+					Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+				instance.transform.SetParent (boardHolder2);
+			}
+		}
+		Instantiate (player, new Vector3 (3, 3, 0f), Quaternion.identity);
+		Instantiate (nurse, new Vector3 (0, 3, 0f), Quaternion.identity);
+		Instantiate (boyTwin, new Vector3 (3, 0, 0f), Quaternion.identity);
+		Instantiate (girlTwin, new Vector3 (4, 0, 0f), Quaternion.identity);
+		Instantiate (music, new Vector3 (7, 2, 0f), Quaternion.identity);
+		Instantiate (librarian, new Vector3 (0, 5, 0f), Quaternion.identity);
+		Debug.Log ("starting upB");
+
+	}
+
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+		public Count (int min, int max)
+		{
+			minimum=min;
+			maximum=max;
+		}
+	}
+		
+	Vector3 RandomPosition ()
+	{
+		int randomIndex = Random.Range (0, gridPositions.Count);
+		Vector3 randomPosition = gridPositions [randomIndex];
+		gridPositions.RemoveAt(randomIndex);
+		return randomPosition;
+	}
+
+	void LayoutObjectAtRandom (GameObject [] outerTileArray, int minimum, int maximum)
+	{
+		int objectCount = Random.Range (minimum, maximum + 1);
+		for(int i=0; i<objectCount; i++)
+		{
+			Vector3 randomPosition = RandomPosition ();
+			GameObject tileChoice = outerTileArray [Random.Range (0, outerTileArray.Length)];
+			Instantiate(tileChoice, randomPosition, Quaternion.identity);
+		}
+	}
+
+	void InitialiseList ()
+	{
+		gridPositions.Clear();
+		}
+
+
+	public void SetupScene (int level)
+	{
+		//SoundManager.instance.MainMusic (innerRoom);
+		BoardSetup ();
+		InitialiseList ();
+	}
+
+
+}
---
+++ Final Project/Assets/Scripts/Win/WinGameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class WinGameManager : MonoBehaviour {
+
+	private WinBoardManager boardScript;
+
+	private int level = 1;
+
+	// Use this for initialization
+	public void Start () {
+		boardScript = GetComponent<WinBoardManager> ();
+		InitGame ();
+	}
+
+	public void InitGame ()
+	{
+		boardScript.SetupScene(level);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
